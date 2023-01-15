8d5f89e6b7d43a5cb550d1d64571e23b2cab52d1
buddhika <buddhika.l@scopicsoftware.com>
2023-01-16 02:17:20 +0530

    setup ph-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import Home from './components/Home';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <Router>
       <Routes>
         <Route path='/' element={<Home tab={0} />} />
         <Route path='/history' element={<Home tab={1} />} />
       </Routes>
-    </Router>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,16 @@ import reportWebVitals from './reportWebVitals';
 import { store, persistor } from './store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>,
