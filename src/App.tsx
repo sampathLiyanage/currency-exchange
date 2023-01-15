0bf91b7d401812e6d9e8c217e0b354013521d688
buddhika <buddhika.l@scopicsoftware.com>
2023-01-15 12:32:26 +0530

    route support

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,15 @@
-import React, { useState, SyntheticEvent } from 'react';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
-
-import theme from './theme';
-import { TabPanel } from './components/Tabs';
-import Grid from '@mui/material/Grid';
-import { CurrencyConverter } from './components/CurrencyConverter';
-import { ThemeProvider } from '@emotion/react';
-import { Paper } from '@mui/material';
+import React from 'react';
+import Home from './components/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
-  const [value, setValue] = useState(0);
-
-  const getTabProps = (index: number) => {
-    return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
-    };
-  };
-
-  const handleChange = (event: SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
   return (
-    <ThemeProvider theme={theme}>
-      <Paper
-        elevation={3}
-        sx={{ backgroundColor: 'white', position: 'sticky', top: 0, zIndex: 1000 }}
-      >
-        <Grid container>
-          <Grid container xs={3} alignItems='center' justifyContent='center'>
-            <CurrencyExchangeIcon sx={{ mr: 1 }} /> Currency Exchange
-          </Grid>
-          <Grid item xs={6}>
-            <Tabs centered value={value} onChange={handleChange} aria-label='basic tabs example'>
-              <Tab label='Currency Converter' {...getTabProps(0)} />
-              <Tab label='View Conversion History' {...getTabProps(1)} />
-            </Tabs>
-          </Grid>
-          <Grid
-            container
-            alignItems='center'
-            justifyContent='center'
-            sx={{ cursor: 'pointer' }}
-            xs={3}
-          >
-            LOGOUT
-          </Grid>
-        </Grid>
-      </Paper>
-
-      <TabPanel value={value} index={0}>
-        <CurrencyConverter />
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-    </ThemeProvider>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home tab={0} />} />
+        <Route path='/history' element={<Home tab={1} />} />
+      </Routes>
+    </Router>
   );
 };
 
---
+++ src/components/Home.tsx
@@ -0,0 +1,73 @@
+import React, { SyntheticEvent } from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
+import { ThemeProvider } from '@emotion/react';
+import { Paper } from '@mui/material';
+import Grid from '@mui/material/Grid';
+
+import theme from '../theme';
+import { TabPanel } from './Tabs';
+import { CurrencyConverter } from './CurrencyConverter';
+import { useNavigate } from 'react-router-dom';
+
+interface HomeProps {
+  tab?: number;
+}
+
+const Home = ({ tab = 0 }: HomeProps) => {
+  const navigate = useNavigate();
+
+  const getTabProps = (index: number) => {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  };
+
+  const handleChange = (event: SyntheticEvent, newValue: number) => {
+    if (newValue === 1) {
+      navigate('/history');
+    } else {
+      navigate('/');
+    }
+  };
+  return (
+    <ThemeProvider theme={theme}>
+      <Paper
+        elevation={3}
+        sx={{ backgroundColor: 'white', position: 'sticky', top: 0, zIndex: 1000 }}
+      >
+        <Grid container>
+          <Grid container xs={3} alignItems='center' justifyContent='center'>
+            <CurrencyExchangeIcon sx={{ mr: 1 }} /> Currency Exchange
+          </Grid>
+          <Grid item xs={6}>
+            <Tabs centered value={tab} onChange={handleChange} aria-label='basic tabs example'>
+              <Tab label='Currency Converter' {...getTabProps(0)} />
+              <Tab label='View Conversion History' {...getTabProps(1)} />
+            </Tabs>
+          </Grid>
+          <Grid
+            container
+            alignItems='center'
+            justifyContent='center'
+            sx={{ cursor: 'pointer' }}
+            xs={3}
+          >
+            LOGOUT
+          </Grid>
+        </Grid>
+      </Paper>
+
+      <TabPanel value={tab} index={0}>
+        <CurrencyConverter />
+      </TabPanel>
+      <TabPanel value={tab} index={1}>
+        Item Two
+      </TabPanel>
+    </ThemeProvider>
+  );
+};
+
+export default Home;
